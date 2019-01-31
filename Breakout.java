e56feaab79429b824f5099284a99f8c8117c2ce9
CS106A <>
2019-01-30 22:41:47 -0800

    {"types":"C","runs":72}

--- Breakout.java
+++ Breakout.java
@@ -151,7 +151,7 @@ public class Breakout extends GraphicsProgram {
 		if (rgen.nextBoolean(0.5)) {
 			vx = -vx;
 		}
-		if (hitLeftWall(ball) || hitRightWall(ball)) {
+		if (ball.getX() <= 0 || hitRightWall(ball)) {
 			vx = -vx;
 		}
 		ball.move(vx, vy);
