cb56d1a8fe9e3d8788d8203526133e7986653e0d
CS106A <>
2019-01-30 22:49:09 -0800

    {"types":"C","runs":81}

--- Breakout.java
+++ Breakout.java
@@ -154,7 +154,7 @@ public class Breakout extends GraphicsProgram {
 		if (ball.getX() <= 0 || ball.getX() >= getWidth() - ball.getWidth()) {
 			vx = -vx;
 		}
-		if (ball.getY() <= 0 || ball.getY() + vy >= getHeight() - ball.getHeight()) {
+		if (ball.getY() <= 0 || ball.getY() + >= getHeight() - ball.getHeight()) {
 			vy = -vy;
 		}	
 		ball.move(vx, vy);
