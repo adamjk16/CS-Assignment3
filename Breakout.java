8a271f5c62b23d23c5b5757affdf4ff92b653ef1
CS106A <>
2019-02-06 01:50:52 -0800

    {"types":"C","runs":307}

--- Breakout.java
+++ Breakout.java
@@ -202,7 +202,7 @@ public class Breakout extends GraphicsProgram {
 		}
 		GObject collider = getCollidingObject();
 		if (collider == paddle) {
-			if (ball.getY() >= getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT - (BALL_RADIUS * 2)) {						
+			if (ball.getY() >= getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT - (BALL_RADIUS * 2) && ball.getY() < getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT - (BALL_RADIUS * 2)) {						
 				vy = -vy;
 			}
 		}	else if (collider != null) {
