b77264533028e9bac28afa59c21784e5943d3f42
CS106A <>
2019-02-05 21:51:04 -0800

    {"types":"C","runs":221}

--- Breakout.java
+++ Breakout.java
@@ -227,8 +227,6 @@ public class Breakout extends GraphicsProgram {
 	private GLabel tryAgain;
 	
 	private void tryAgain() {
-		remove(ball);
-		remove(paddle);
 		tryAgain = new GLabel ("Click to Try Again!");
 		tryAgain.setFont ("SansSerif-28");
 		double x = getWidth()/2 - tryAgain.getWidth()/2;
